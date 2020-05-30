984a3f88dfd42437bda9062a3cc133cbb72997fd
varun chakravarthy <varun chakravarthy@LAPTOP-NVPEE1AD.tx.rr.com>
2020-05-30 02:14:38 -0500

    next commit

--- src/main/java/com/spring/mysql/api/controller/TicketController.java
+++ src/main/java/com/spring/mysql/api/controller/TicketController.java
@@ -1,10 +1,15 @@
 package com.spring.mysql.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,16 +40,15 @@ public class TicketController {
 		return "ticket booked Sucessfuly: " + tickets.size();
 	}
 
-	
 	// retrive values
-	
+
 	@GetMapping("/getTickets")
 	public List<Ticket> getTickets() {
 		return (List<Ticket>) dao.findAll();
 	}
-	
+
 	// modify values
-	
+
 	/*
 	 * @GetMapping("/getTickets") public List<Ticket> getModifiedTickets() { return
 	 * null;
@@ -59,4 +63,37 @@ public class TicketController {
 	 * 
 	 * 
 	 */
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable int id) {
+		if (dao.findOne(id) != null) {
+			dao.delete(id);
+
+		} else {
+			System.out.println("Id not present");
+			return "id not present";
+		}
+
+		// System.out.println("id has been deleted");
+		return "This Id has been deleted " + id;
+	}
+
+	@PutMapping("/{id}")
+	public String updateStudent(@RequestBody Ticket ticket, @PathVariable int id) {
+		Optional<Ticket> studentOptional = Optional.ofNullable(dao.findOne(id));
+
+		if (!studentOptional.isPresent())
+		// return ResponseEntity.notFound().build();
+		{
+			ticket.setId(id);
+			// ticket.setAmount(amount);
+			dao.save(ticket);
+		} else {
+			return "Entered id is not present in DB";
+		}
+		return "Entered " + id + " is not present in DB";
+
+		// return ResponseEntity.noContent().build();
+
+	}
+
 }
